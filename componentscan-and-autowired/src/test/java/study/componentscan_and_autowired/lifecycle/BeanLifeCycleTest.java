38361aaa6c1b038c7e324f1ddef14375feccf945
yjk490 <yjk490@gmail.com>
2023-07-23 16:57:32 +0900

    feat : 빈 생명주기 콜백

---
+++ componentscan-and-autowired/src/test/java/study/componentscan_and_autowired/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,27 @@
+package study.componentscan_and_autowired.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+    @Test
+    void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient networkClient = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("https://github.com/yjk490");
+            return networkClient;
+        }
+    }
+}
