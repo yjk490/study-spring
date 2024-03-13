f7052924c0b8b1dee2ab417cf2ae916310329d26
yjk490 <yjk490@gmail.com>
2024-03-13 23:34:11 +0900

    faet: HTTP 응답 다루기

---
+++ springmvc/src/main/java/study/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,23 @@
+package study.springmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1() {
+        ModelAndView modelAndView = new ModelAndView("response/hello")
+                .addObject("data","Hello");
+        return modelAndView;
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model) {
+        model.addAttribute("data", "Hello2");
+        return "response/hello";
+    }
+}
