5cdf61ebacb113e76b605f62d54a0e118e0e871c
yjk490 <yjk490@gmail.com>
2024-03-17 21:41:33 +0900

    feat: item-service 프로젝트 추가

---
+++ item-servic/src/main/java/study/itemservice/ItemServicApplication.java
@@ -0,0 +1,13 @@
+package study.itemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemServicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemServicApplication.class, args);
+	}
+
+}
