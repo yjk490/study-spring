80bb7c4ab5190ebf4f4420d40380c9793bf52beb
yjk490 <yjk490@gmail.com>
2024-06-23 17:39:22 +0900

    스프링 부트 예외 처리 기본

---
+++ exception/src/main/java/hello/exception/servlet/HomeController.java
@@ -0,0 +1,16 @@
+package hello.exception.servlet;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
